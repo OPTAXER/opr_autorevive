b5f1fefaa09ad8fc04e76dabcd8be1bc46167435
Optaxer | OPR <84898728+OPTAXER@users.noreply.github.com>
2023-02-17 16:06:57 +0530

    v1

---
+++ opr_autorevive/client.lua
@@ -0,0 +1,20 @@
+local isDead = false
+
+AddEventHandler('playerSpawned', function()
+    isDead = false
+end)
+
+AddEventHandler('esx:onPlayerDeath', function()
+    --exports['OPRNotify']:Alert("Dr Opr💖", "You will auto revive in 4 min", 8000, 'info')
+    ESX.ShowNotification("Dr Opr💖: You will auto revive in 4 min", "error", 10000)
+    isDead = true
+    reviveTimer()
+end)
+function reviveTimer()
+    Citizen.SetTimeout(240000, function()
+        if isDead then
+            TriggerEvent('esx_ambulancejob:revive')
+            isDead = false
+        end
+    end)
+end
---
+++ opr_autorevive/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+
+game 'gta5'
+author 'Optaxer'
+
+description 'Auto revive script for esx legacy'
+
+client_scripts {
+	'client/main.lua',
+}
